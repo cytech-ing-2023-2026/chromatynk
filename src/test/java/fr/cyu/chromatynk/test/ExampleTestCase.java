9413fb7d4258d2ea591a33a1f387d55fbca01745
Raphaël Fromentin <42907886+Iltotore@users.noreply.github.com>
2024-05-07 09:41:06 +0200

    test: Setup JUnit (#10)

---
+++ src/test/java/fr/cyu/chromatynk/test/ExampleTestCase.java
@@ -0,0 +1,20 @@
+package fr.cyu.chromatynk.test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * A test suite example.
+ */
+class ExampleTestCase {
+
+    /**
+     * A test example.
+     */
+    @Test
+    void addition() {
+        assertEquals(2, 1+1);
+    }
+
+}
