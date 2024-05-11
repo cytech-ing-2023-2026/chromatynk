c3df69cabbbbf74985743c5bcf912d0a4b3dff1f
JordanViknar <74505993+JordanViknar@users.noreply.github.com>
2024-05-11 14:35:52 +0200

    refactor: Organize code (& white canvas)

--- src/main/java/fr/cyu/chromatynk/editor/CodeEditor.java
+++ src/main/java/fr/cyu/chromatynk/editor/CodeEditor.java
@@ -12,6 +12,7 @@ import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
@@ -21,79 +22,83 @@ import java.io.IOException;
 
 public class CodeEditor extends Application {
 
-    @Override
-    public void start(Stage primaryStage) {
-        // Création de la zone de texte pour l'éditeur de code
+	@Override
+	public void start(Stage primaryStage) {
 
-        TextArea codeArea = new TextArea();
-        codeArea.setPrefWidth(300);
-        codeArea.setPrefHeight(200);
-        codeArea.setWrapText(true); // Activer le retour à la ligne automatique
+		// Create text zone
+		TextArea codeArea; {
+			codeArea = new TextArea();
+			codeArea.setPrefWidth(300);
+	 		codeArea.setPrefHeight(200);
+			codeArea.setWrapText(true);
+		}
 
-        // Création du bouton supprimer
-        Button deleteButton = new Button("Supprimer");
-        deleteButton.setOnAction(e -> codeArea.clear()); // Action du bouton pour supprimer le texte
+		// Execution / Deletion container
+		HBox buttonBox; {
+			Button deleteButton = new Button("Supprimer");
+			deleteButton.setOnAction(e -> codeArea.clear()); // Action du bouton pour supprimer le texte
+	
+			Button validateButton = new Button("Exécuter");
+			// This is where we run the draw system.
 
+			buttonBox = new HBox(deleteButton, validateButton);
+			buttonBox.setPadding(new Insets(10));
+			buttonBox.setSpacing(10);
+		}
 
+		// Create drawing zone for the result
+		Canvas canvas; {
+			canvas = new Canvas(300, 200);
+			// Configure white background
+			GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
+			graphicsContext.setFill(Color.WHITE);
+			graphicsContext.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
+		}
 
-        // Création boutton "Valider"
-        Button validateButton = new Button("Valider");
-        //définir l'action (A completer)
+		// Create drawing zone container
+		BorderPane resultPane; {
+			resultPane = new BorderPane();
+			resultPane.setCenter(canvas);
+		};
 
-        // Création du conteneur pour le bouton supprimer et valider
-        HBox buttonBox = new HBox(deleteButton, validateButton);
-        buttonBox.setPadding(new Insets(10));
-        buttonBox.setSpacing(10);
+		// Création de la barre de menu
+		MenuBar menuBar; {
+			// Création du menu Fichier pour pouvoir enregistrer et ouvrir des images
+			Menu fileMenu; {
+				MenuItem openMenuItem = new MenuItem("Ouvrir");
+				MenuItem saveAsPngMenuItem = new MenuItem("Enregistrer sous");
+	
+				// Action pour le menu Ouvrir (à completer)
+	
+				// Action pour le menu Enregistrer en PNG (A completer)
+	
+				fileMenu = new Menu("Fichier");
+				// Ajout des éléments au menu Fichier
+				fileMenu.getItems().addAll(openMenuItem, saveAsPngMenuItem);
+			}
+			menuBar = new MenuBar();
+			menuBar.getMenus().add(fileMenu);
+		}
 
-        // Création de la zone de dessin pour le résultat
-        Canvas canvas = new Canvas(300, 200);
-        GraphicsContext gc = canvas.getGraphicsContext2D();
+		// Création de la scène
+		Scene scene; {
+			// Création du conteneur principal
+			BorderPane root = new BorderPane();
+			root.setLeft(codeArea);
+			root.setBottom(buttonBox);
+			root.setRight(resultPane);
+			root.setTop(menuBar);
 
+			scene = new Scene(root, 600, 400);
+		}
 
-        // Création du conteneur pour la zone de dessin
-        BorderPane resultPane = new BorderPane();
-        resultPane.setCenter(canvas);
+		// Configuration de la scène et affichage de la fenêtre
+		primaryStage.setTitle("Chromatynk");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
 
-        // Création du menu Fichier pour pouvoir enregistrer et ouvrir des images
-        Menu fileMenu = new Menu("Fichier");
-        MenuItem openMenuItem = new MenuItem("Ouvrir");
-        MenuItem saveAsPngMenuItem = new MenuItem("Enregistrer sous");
-
-        // Action pour le menu Ouvrir (à completer)
-
-
-        // Action pour le menu Enregistrer en PNG (A completer)
-
-
-        // Ajout des éléments au menu Fichier
-        fileMenu.getItems().addAll(openMenuItem, saveAsPngMenuItem);
-
-        // Création de la barre de menu
-        MenuBar menuBar = new MenuBar();
-        menuBar.getMenus().add(fileMenu);
-
-
-
-        // Création du conteneur principal
-        BorderPane root = new BorderPane();
-        root.setPadding(new Insets(10));
-        root.setLeft(codeArea);
-        root.setBottom(buttonBox);
-        root.setRight(resultPane);
-        root.setTop(menuBar);
-
-
-        // Création de la scène
-        Scene scene = new Scene(root, 600, 400);
-
-        // Configuration de la scène et affichage de la fenêtre
-        primaryStage.setTitle("Cromatynk");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
-
-
-    public static void main(String[] args) {
-        launch(args);
-    }
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
