e8cd25ae8661405cd4731907215465016149f200
JordanViknar <74505993+JordanViknar@users.noreply.github.com>
2024-05-12 12:52:25 +0200

    refactor: Move UI to FXML file

--- src/main/java/fr/cyu/chromatynk/editor/CodeEditor.java
+++ src/main/java/fr/cyu/chromatynk/editor/CodeEditor.java
@@ -1,133 +1,28 @@
 package fr.cyu.chromatynk.editor;
 
 import javafx.application.Application;
-import javafx.geometry.Insets;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.control.Button;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.TextArea;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-import javafx.scene.layout.Priority;
-import javafx.scene.control.Label;
-import javafx.scene.layout.Region;
+
+import java.io.IOException;
 
 public class CodeEditor extends Application {
 
 	@Override
-	public void start(@SuppressWarnings("exports") Stage primaryStage) {
-		// Interactive elements
-		TextArea codeArea = new TextArea();
-		Canvas canvas = new Canvas(400, 400);
-		Label infoLabel = new Label("INFO - Message d'information utile...");
-		Label statusLabel = new Label("Dessin en cours...");
+	@SuppressWarnings("exports")
+	public void start(Stage primaryStage) throws IOException {
+	   	// Prepare and load the FXML file
+		Parent root; {
+			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/CodeEditor.fxml"));
+			fxmlLoader.setController(new CodeEditorController(primaryStage));
+			root = fxmlLoader.load();
+		};
 
-		// Create scene
-		Scene scene; {
-			// Create main container
-			BorderPane root; {
-				root = new BorderPane();
-
-				// Create menu bar
-				MenuBar menuBar; {
-					// Create File menu to open and save drawing instructions
-					Menu fileMenu; {
-						MenuItem openMenuItem = new MenuItem("Ouvrir");
-						MenuItem saveFileMenuItem = new MenuItem("Enregistrer sous");
-			
-						// The actions for both menu items
-						FileMenuController fileMenuController = new FileMenuController(primaryStage, codeArea);
-						openMenuItem.setOnAction(e -> fileMenuController.openFile());
-						saveFileMenuItem.setOnAction(e -> fileMenuController.saveFile());
-			
-						fileMenu = new Menu("Fichier");
-						fileMenu.getItems().addAll(openMenuItem, saveFileMenuItem);
-					}
-					// Create Image menu to save the drawing as an image
-					Menu imageMenu; {
-						MenuItem saveImageMenuItem = new MenuItem("Enregistrer sous");
-						imageMenu = new Menu("Image");
-						imageMenu.getItems().addAll(saveImageMenuItem);
-					}
-					// Create about menu
-					Menu helpMenu; {
-						MenuItem helpMenuItem = new MenuItem("À propos");
-						helpMenu = new Menu("Aide");
-						helpMenu.getItems().add(helpMenuItem);
-					}
-
-					menuBar = new MenuBar();
-					menuBar.getMenus().addAll(fileMenu, imageMenu, helpMenu);
-				}
-				root.setTop(menuBar);
-				
-				HBox mainArea; {
-					VBox userControlArea; {
-						// Configure text zone
-						{
-							codeArea.setPrefWidth(300);
-							codeArea.setPrefHeight(300);
-							codeArea.setWrapText(true);
-							VBox.setVgrow(codeArea, Priority.ALWAYS);
-						}
-
-						// Execution / Deletion container
-						HBox buttonBox; {
-							Button validateButton = new Button("Exécuter");
-							// This is where we run the draw system.
-
-							Button deleteButton = new Button("Supprimer");
-							deleteButton.setOnAction(e -> codeArea.clear());
-
-							buttonBox = new HBox(validateButton, deleteButton);
-							buttonBox.setPadding(new Insets(10));
-							buttonBox.setSpacing(10);
-						}
-						userControlArea = new VBox(codeArea, buttonBox);
-					}
-
-					// Create drawing zone container
-					BorderPane resultPane; {
-						// Configure drawing zone
-						{
-							// Configure white background
-							GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
-							graphicsContext.setFill(Color.WHITE);
-							graphicsContext.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
-						}
-						resultPane = new BorderPane();
-						resultPane.setCenter(canvas);
-						HBox.setHgrow(resultPane, Priority.ALWAYS);
-					};
-					mainArea = new HBox(userControlArea, resultPane);
-					mainArea.setSpacing(10);
-					mainArea.setPadding(new Insets(10));
-				}
-				root.setCenter(mainArea);
-
-				// Set bottom status bar
-				HBox statusBox; {
-					Region spacer = new Region();
-					HBox.setHgrow(spacer, Priority.ALWAYS); // allow it to grow horizontally
-
-					statusBox = new HBox(infoLabel, spacer, statusLabel);
-					statusBox.setPadding(new Insets(10));
-					statusBox.setSpacing(10);
-				}
-				root.setBottom(statusBox);
-			}
-			scene = new Scene(root, 800, 500);
-		}
-
+		// Set the stage
 		primaryStage.setTitle("Chromatynk");
-		primaryStage.setScene(scene);
+		primaryStage.setScene(new Scene(root, 800, 500));
 		primaryStage.show();
 	}
 
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,13 +1,9 @@
 module fr.cyu {
-
     requires javafx.controls;
-    requires java.desktop;
+	requires javafx.fxml;
     exports fr.cyu.chromatynk;
     exports fr.cyu.chromatynk.ast.statement;
     exports fr.cyu.chromatynk.ast.expr;
     exports fr.cyu.chromatynk.editor;
-
-
-
-
+	opens fr.cyu.chromatynk.editor to javafx.fxml;
 }
