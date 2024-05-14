e03b7b683c8b1053d0ea4c569baeb2adc597a232
Iltotore <rafbodaha@gmail.com>
2024-05-14 15:57:40 +0200

    feat: Add platform-agnostic canvas

--- src/main/java/fr/cyu/chromatynk/eval/Canvas.java
+++ src/main/java/fr/cyu/chromatynk/eval/Canvas.java
@@ -1,11 +1,102 @@
 package fr.cyu.chromatynk.eval;
 
-import javafx.scene.paint.Color;
+public class Canvas {
 
-public record Canvas(Color[][] pixels, int width, int height) {
+    //Could have been a record but decided to make a POJO to explicitly tell this class is mutable (because arrays are).
+    private final int width;
+    private final int height;
+    private final Color[][] pixels;
 
-    public Canvas withLine(int x, int y, int width, int height) {
-        //TODO
-        return null;
+    public Canvas(int width, int height, Color[][] pixels) {
+        this.width = width;
+        this.height = height;
+        this.pixels = pixels;
+    }
+
+    public Canvas(int width, int height) {
+        this(width, height, new Color[height][width]);
+        fill(new Color(1, 1, 1));
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public Color[][] getPixels() {
+        return pixels;
+    }
+
+    public boolean contains(int x, int y) {
+        return x >= 0 && x < width && y >= 0 && y < height;
+    }
+
+    public void drawPixel(int x, int y, Color color) {
+        if (contains(x, y)) pixels[y][x] = color;
+    }
+
+    public void drawLine(int startX, int startY, int endX, int endY, Color color) {
+        int dx = Math.abs(endX - startX);
+        int sx = startX < endX ? 1 : -1;
+
+        int dy = -Math.abs(endY - startY);
+        int sy = startY < endY ? 1 : -1;
+
+        int err = dx + dy, e2;
+
+        int x = startX;
+        int y = startY;
+
+        while (x != endX || y != endY) {
+            drawPixel(x, y, color);
+
+            e2 = 2 * err;
+
+            if (e2 > dy) {
+                err += dy;
+                x += sx;
+            }
+
+            if (e2 < dx) {
+                err += dx;
+                y += sy;
+            }
+        }
+
+        drawPixel(x, y, color);
+    }
+
+    public void fill(Color color) {
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) pixels[y][x] = color;
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) result.append(pixels[y][x].toANSI()).append(' ');
+            result.append("\u001B[0m\n");
+        }
+        return result.substring(0, result.length() - 1);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(!(obj instanceof Canvas canvas)) return false;
+
+        if(width != canvas.getWidth() || height != canvas.getHeight()) return false;
+
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                if (pixels[y][x] != canvas.pixels[y][x]) return false;
+            }
+        }
+
+        return true;
     }
 }
---
+++ src/main/java/fr/cyu/chromatynk/eval/Color.java
@@ -0,0 +1,46 @@
+package fr.cyu.chromatynk.eval;
+
+import java.util.Map;
+
+public record Color(double red, double green, double blue) {
+
+    public double getBrightness() {
+        return 0.257 * red + 0.504 * green + 0.098 * blue + 16;
+    }
+
+    public double getBlueProjection() {
+        return -0.148 * red - 0.291 * green + 0.439 * blue + 128;
+    }
+
+    public double getRedProjection() {
+        return 0.439 * red - 0.368 * green - 0.071 * blue + 128;
+    }
+
+    private double distanceSquaredTo(Color color) {
+        double dy = Math.abs(getBrightness() - color.getBrightness());
+        double du = Math.abs(getBlueProjection() - color.getBlueProjection());
+        double dv = Math.abs(getRedProjection() - color.getRedProjection());
+
+        return dy * dy + du * du + dv * dv;
+    }
+
+    public String toANSI() {
+        return ANSI_COLORS
+                .entrySet()
+                .stream()
+                .min((a, b) -> Double.compare(distanceSquaredTo(a.getKey()), distanceSquaredTo(b.getKey())))
+                .map(Map.Entry::getValue)
+                .get();
+    }
+
+    private static final Map<Color, String> ANSI_COLORS = Map.ofEntries(
+            Map.entry(new Color(0, 0, 0), "\u001B[40m"),
+            Map.entry(new Color(1, 0, 0), "\u001B[41m"),
+            Map.entry(new Color(0, 1, 0), "\u001B[42m"),
+            Map.entry(new Color(1, 1, 0), "\u001B[43m"),
+            Map.entry(new Color(0, 0, 1), "\u001B[44m"),
+            Map.entry(new Color(1, 0, 1), "\u001B[45m"),
+            Map.entry(new Color(0, 1, 1), "\u001B[46m"),
+            Map.entry(new Color(1, 1, 1), "\u001B[47m")
+    );
+}
