bf3bb5621a399423e5949412ff2d254f62515eb2
JordanViknar <74505993+JordanViknar@users.noreply.github.com>
2024-05-14 17:41:22 +0200

    feat: Step-by-step mode UI (& missing comments)

--- src/main/java/fr/cyu/chromatynk/editor/CodeEditor.java
+++ src/main/java/fr/cyu/chromatynk/editor/CodeEditor.java
@@ -8,10 +8,21 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+/**
+ * The main JavaFX application loader for the code editor
+ * @author JordanViknar
+ * @see CodeEditorController
+ */
 public class CodeEditor extends Application {
 
 	@Override
 	@SuppressWarnings("exports")
+	/**
+	 * Start the JavaFX code editor application
+	 * @param primaryStage the primary stage
+	 * @throws IOException if the FXML file cannot be loaded
+	 * @see CodeEditorController
+	 */
 	public void start(Stage primaryStage) throws IOException {
 	   	// Prepare and load the FXML file
 		Parent root; {
@@ -26,6 +37,10 @@ public class CodeEditor extends Application {
 		primaryStage.show();
 	}
 
+	/**
+	 * Launch the JavaFX application
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		launch(args);
 	}
--- src/main/java/fr/cyu/chromatynk/editor/CodeEditorController.java
+++ src/main/java/fr/cyu/chromatynk/editor/CodeEditorController.java
@@ -3,17 +3,25 @@ package fr.cyu.chromatynk.editor;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.CheckMenuItem;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.scene.layout.HBox;
 
+/**
+ * The controller for the code editor
+ * @author JordanViknar
+ */
 public class CodeEditorController {
+	// The 2 main elements
 	@FXML
 	private TextArea codeArea;
 	@FXML
 	private Canvas canvas;
+
+	// Bottom bar
 	@FXML
 	private Label infoLabel;
 	@FXML
@@ -21,12 +29,22 @@ public class CodeEditorController {
 	@FXML
 	private HBox bottomBar;
 
+	// Step-by-step mode
+	@FXML
+	private CheckMenuItem stepByStepCheckbox;
+	@FXML
+	private HBox stepByStepControls;
+
 	private final Stage primaryStage;
 	private FileMenuController fileMenuController;
 
 	@SuppressWarnings("exports")
 	public CodeEditorController(Stage primaryStage) {this.primaryStage = primaryStage;}
 	
+	/**
+	 * This function is used to setup the UI elements of the code editor Java-side.
+	 * Basically : it's for things we cannot do in FXML.
+	 */
 	public void initialize() {
 		this.fileMenuController = new FileMenuController(primaryStage, codeArea);
 
@@ -37,9 +55,16 @@ public class CodeEditorController {
 
 		// Makes the bottom bar's background slightly darker
 		bottomBar.setStyle("-fx-background-color: rgba(0, 0, 0, 0.07);");
+
+		// Only show relevant UI when in step-by-step mode
+		stepByStepControls.setVisible(stepByStepCheckbox.isSelected());
+		stepByStepCheckbox.selectedProperty().addListener((observable, oldValue, newValue) -> {
+			stepByStepControls.setVisible(newValue);
+		});
 	}
 
 	public void openFile() {fileMenuController.openFile();}
 	public void saveFile() {fileMenuController.saveFile();}
 	public void clearTextArea() {codeArea.clear();}
+	public void quit() {primaryStage.close();}
 }
